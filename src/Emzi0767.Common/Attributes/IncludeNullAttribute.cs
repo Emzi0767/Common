e602aa0e623e8358514142177894e17374f8eca1
Mateusz Brawański <emzi0767@gmail.com>
2021-02-22 19:02:47 +0100

    Obsolete things adequately replaced by DataContract attributes.

--- src/Emzi0767.Common/Attributes/IncludeNullAttribute.cs
+++ src/Emzi0767.Common/Attributes/IncludeNullAttribute.cs
@@ -22,6 +22,7 @@ namespace Emzi0767.Serialization
     /// <para>Specifies that if the value of the field or property is null, it should be included in the serialized data.</para>
     /// <para>This alters the default behaviour of ignoring nulls.</para>
     /// </summary>
+    [Obsolete("Use [DataMember] with EmitDefaultValue = true.")]
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public sealed class IncludeNullAttribute : Attribute
     { }
